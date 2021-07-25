c77f1f9c0f46867f5219ce5ee6edc2584d45b79d
FrightenedFox <vitalij20358@gmail.com>
2021-07-25 20:15:53 +0200

    Mutation system has been added to the mod-E-GAN/tensorflow_original_half_e_gan.py

--- mod-E-GAN/tensorflow_original_half_e_gan.py
+++ mod-E-GAN/tensorflow_original_half_e_gan.py
@@ -5,9 +5,10 @@ import imageio
 import matplotlib.pyplot as plt
 import numpy as np
 import os
-import PIL
 from tensorflow.keras import layers
+from datetime import datetime
 import time
+from bit_operations import BitOps
 
 
 def make_generator_model():
@@ -64,15 +65,38 @@ class TensorHalfEGAN:
                  batch_size=256,
                  epochs=50,
                  noise_dim=100,
-                 num_examples_to_generate=16):
+                 enable_mutations=True,
+                 num_examples_to_generate=16,
+                 n_mut=50,
+                 mut_prob=0.002):
 
         self.buffer_size = buffer_size
         self.batch_size = batch_size
         self.epochs = epochs
         self.noise_dim = noise_dim
         self.num_examples_to_generate = num_examples_to_generate
-        self.anim_file = 'images/dcgan.gif'
         self.show_images = False
+
+        self.enable_mutations = enable_mutations
+        self.n_mut = n_mut
+        self.mut_prob = mut_prob
+        self.n_successful_mutations, self.n_mutations = 0, 0
+        self.layers_to_mutate = [
+            "dense",
+            "conv2d_transpose",
+            "conv2d_transpose_1",
+            "conv2d_transpose_2",
+        ]
+
+        self.model_name_suffix = (f"_m{self.enable_mutations}"
+                                  f"_ep{self.epochs}_nm{self.n_mut}"
+                                  f"_mp{self.mut_prob}")
+        self.unique_model_name = (
+            f"TF_orig_t{datetime.now().strftime('%m%d_%H%M%S')}"
+            f"{self.model_name_suffix}"
+        )
+        os.makedirs(f"images/{self.unique_model_name}", exist_ok=True)
+        self.anim_file = f"images/{self.unique_model_name}/dcgan.gif"
 
         self.seed = tf.random.normal([num_examples_to_generate, noise_dim])
         self.train_dataset = self.load_data()
@@ -147,6 +171,56 @@ class TensorHalfEGAN:
             self.discriminator.trainable_variables
         ))
 
+    def create_mutations(self):
+        print(f"Calculating mutations with p = {self.mut_prob}")
+        for layer_id, layer in enumerate(self.generator.layers):
+            if layer.name in self.layers_to_mutate:
+                weights = layer.get_weights()
+                weights_len = len(weights)
+                print(f"Layer # {layer_id} --- {layer.name}")
+                template = [None for _ in range(weights_len)]
+                mutated_layer_params = [template.copy()
+                                        for _ in range(self.n_mut)]
+                for param_id, param in enumerate(weights):
+                    in_shape = param.shape
+                    mut_engine = BitOps(param.flatten())
+                    mut_engine.mutate(n_mut=self.n_mut, prob=self.mut_prob)
+                    for mut_id in range(self.n_mut):
+                        mutated_layer_params[mut_id][param_id] = mut_engine. \
+                            mutations[mut_id].reshape(in_shape)
+                    del mut_engine
+                self.compare_mutations(mutated_layer_params, layer_id)
+
+    def compare_mutations(self, mutated_weights, layer_id, n_tests=16):
+        mut_loss_res = np.empty(len(mutated_weights))
+        noise = tf.random.normal([n_tests, self.noise_dim])
+        original_weights = self.generator.layers[layer_id].get_weights()
+        gen_imgs = self.generator(noise, training=False)
+        fake_out = self.discriminator(gen_imgs, training=False)
+        curr_loss = self.cross_entropy(tf.zeros_like(fake_out), fake_out)
+        for mut_id, weights in enumerate(mutated_weights):
+            self.generator.layers[layer_id].set_weights(weights)
+            gen_imgs = self.generator(noise, training=False)
+            fake_out = self.discriminator(gen_imgs, training=False)
+            mut_loss_res[mut_id] = self.cross_entropy(tf.zeros_like(fake_out),
+                                                      fake_out)
+        max_id = np.argmax(mut_loss_res)
+        to_print_and_log = np.copy(mut_loss_res)
+        to_print_and_log[::-1].sort()
+
+        print(f"Current loss {curr_loss:.4}\n"
+              f"Best mutations (larger is better):\t",
+              to_print_and_log[:5])
+        self.n_mutations += 1
+        if mut_loss_res[max_id] > curr_loss:
+            print("Applying new parameters!\n")
+            self.generator.layers[layer_id].set_weights(
+                mutated_weights[int(max_id)])
+            self.n_successful_mutations += 1
+        else:
+            print("Mutation unsuccessful, keeping old parameters.\n")
+            self.generator.layers[layer_id].set_weights(original_weights)
+
     def generate_and_save_images(self, epoch, test_input):
         # Notice `training` is set to False.
         # This is so all layers run in inference mode (batchnorm).
@@ -158,7 +232,7 @@ class TensorHalfEGAN:
             plt.imshow(predictions[i, :, :, 0] * 127.5 + 127.5, cmap='gray')
             plt.axis('off')
 
-        plt.savefig(f'images/image_at_epoch_{epoch:04d}.png')
+        plt.savefig(f'images/{self.unique_model_name}/image_{epoch:04d}.png')
         if self.show_images:
             plt.show()
 
@@ -177,7 +251,13 @@ class TensorHalfEGAN:
             if (epoch + 1) % 15 == 0:
                 self.checkpoint.save(file_prefix=self.checkpoint_prefix)
 
-            print(f'Time for epoch {epoch + 1} is {time.time() - start} sec')
+            if self.enable_mutations:
+                self.create_mutations()
+                print(f"Mutation success rate: {self.n_successful_mutations}/"
+                      f"{self.n_mutations}")
+
+            print(f"{8*'-'}\tEpoch {epoch + 1} time: {time.time() - start} sec")
+
 
         # Generate after the final epoch
         self.generate_and_save_images(self.epochs,
@@ -185,7 +265,7 @@ class TensorHalfEGAN:
 
     def make_animation(self):
         with imageio.get_writer(self.anim_file, mode='I') as writer:
-            filenames = glob.glob('images/image*.png')
+            filenames = glob.glob(f"images/{self.unique_model_name}/image*.png")
             filenames = sorted(filenames)
             for filename in filenames:
                 image = imageio.imread(filename)
@@ -193,6 +273,9 @@ class TensorHalfEGAN:
 
 
 if __name__ == "__main__":
-    gan = TensorHalfEGAN(epochs=5,)
+    gan = TensorHalfEGAN(epochs=50,
+                         mut_prob=0.002,
+                         n_mut=50,
+                         enable_mutations=True)
     gan.train()
     gan.make_animation()
