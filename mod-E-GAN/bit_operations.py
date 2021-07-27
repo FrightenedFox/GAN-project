fb73035861167d9846259a78368188cab8b289c5
FrightenedFox <vitalij20358@gmail.com>
2021-07-27 12:22:46 +0200

    mod-E-GAN/bit_operations.py
    - code has been fully documented in the Numpy doc style;
    - selection algorithm has been fixed.

--- mod-E-GAN/bit_operations.py
+++ mod-E-GAN/bit_operations.py
@@ -2,37 +2,74 @@ import numpy as np
 
 
 class BitOps:
+    """Generates mutations and selections of the input vector.
+
+    Parameters
+    ----------
+    original : 1D array-like
+        1D list or numpy array input
+
+    Attributes
+    ----------
+    original : numpy.array
+        Input vector represented as the numpy array
+    mutations : numpy.array
+        An array where all the mutations are stored
+
+    Methods
+    -------
+    mutate(n_mut, apply_selection=False, n_selections=1, **kwargs)
+        Creates mutations of the original input array
+
+    Examples
+    --------
+    >>> bit_ops = BitOps(np.array([0.234, -1.23, 12.625]))
+    >>> print(bit_ops.mutate(n_mut=3, prob=0.05))
+    [[ 0.23400001 -1.23       12.68751527]
+     [ 0.23399952 -1.23000048 12.70709229]
+     [ 0.234      -1.23000191 12.625     ]]
+
+    >>> bit_ops = BitOps(np.array([0.234, -1.23, 12.625]))
+    >>> print(bit_ops.mutate(n_mut=3, prob=0.1,
+    >>>                      apply_selection=True, n_selections=1))
+    [[ 0.98385157 -1.24571748 25.00109875]
+     [ 0.2294679  -0.61695307 12.62555313]
+     [ 0.23400015 -1.2300276  12.62524414]
+     [ 1.0241443  -1.85433307 34.62207248]
+     [ 0.18941724 -0.60176597  3.0043392 ]
+     [ 0.23375808 -0.6365991  12.62548436]]
+    """
+
     def __init__(self, original):
         self.original = np.array(original)
-        self._int_orig = self.float2int(self.original)
+        self._int_orig = self._float2int(self.original)
         self.mutations = None
         self._mut_masks = None
-        self.in_shape = self.original.shape
+        self._in_shape = self.original.shape
 
     @staticmethod
-    def float2int(in_val: np.array) -> np.array:
-        """ Convert float to 64-bit binary integer
+    def _float2int(in_val: np.array) -> np.array:
+        """ Convert float numbers to 64-bit integers
 
         Parameters
         ----------
-            in_val : numpy.array
-                Float number to transform
+        in_val : numpy.array
+            List of the float numbers to transform
 
         Returns
         -------
         np.array
-            binary integer representation of f (IEEE 754 standard)
         """
         return np.frombuffer(in_val.astype("float64").tobytes(), dtype="int64")
 
     @staticmethod
-    def int2float(in_val: np.array) -> np.array:
-        """ Convert binary integer to a float.
+    def _int2float(in_val: np.array) -> np.array:
+        """ Convert 64-bit integers to float numbers
 
         Parameters
         ----------
-            in_val : numpy.array
-                Binary string to transform.
+        in_val : numpy.array
+            List of the integer numbers to transform
 
         Returns
         -------
@@ -41,31 +78,33 @@ class BitOps:
         return np.frombuffer(in_val.astype("int64").tobytes(), dtype="float64")
 
     def _gen_mut_masks(self, n_mut=5, prob=0.05, length=56, chunk_s=8):
+        # Generating chunks and calculating their probabilities
         probs = (1 - prob, prob)
         num_list = np.arange(2 ** chunk_s, dtype="int64")
-        bin_list = [f'{d:0{chunk_s}b}' for d in range(2 ** chunk_s)]
-        prob_list = [probs[0] ** d.count("0") * probs[1] ** d.count("1")
-                     for d in bin_list]
+        bin_list = np.array([f'{d:0{chunk_s}b}' for d in range(2 ** chunk_s)])
+        ones_count = np.char.count(bin_list, "1")
+        prob_list = probs[0] ** (chunk_s - ones_count) * probs[1] ** ones_count
+
+        # Sampling chunks and concatenating them
         n_masks = n_mut * len(self.original)
         self._mut_masks = np.zeros(n_masks, dtype="int64")
         for i in range(int(length / chunk_s)):
-            next_chunk = np.random.choice(
-                num_list, size=n_masks, p=prob_list
-            )
+            next_chunk = np.random.choice(num_list, size=n_masks, p=prob_list)
             next_chunk *= 2 ** (i * chunk_s)
             self._mut_masks += next_chunk
+
         self._mut_masks = self._mut_masks.reshape((n_mut, len(self.original)))
         return self._mut_masks
 
     @staticmethod
-    def pseudo_selection(in_arr, n_selections=1):
+    def _pseudo_selection(in_arr, n_selections=1):
         rng = np.random.default_rng()
         skeleton = np.tile(in_arr, n_selections).astype("float64")
         skeleton_1, skeleton_2 = skeleton.copy(), skeleton.copy()
         rng.shuffle(skeleton_1)
         rng.shuffle(skeleton_2)
         skeleton += rng.uniform(
-            size=in_arr.shape[0] * n_selections
+            size=(in_arr.shape[0] * n_selections, 1)
         ) * (skeleton_1 - skeleton_2)
         out_arr = skeleton.reshape(in_arr.shape[0] * n_selections,
                                    *in_arr.shape[1:])
@@ -78,37 +117,37 @@ class BitOps:
         ----------
         n_mut : int
             number of mutations
-        apply_selection : bool
-            whether to apply selection over the mutated values
-        n_selections : int
-            number of selections to apply over each mutation
+        apply_selection : bool, optional
+            Whether to apply selection over the mutated values. Default False.
+        n_selections : int, optional
+            number of selections to apply over each mutation. Default 1.
         kwargs:
-            prob : float
-                probability of mutation
+            prob : float, optional
+                Probability of mutation. Default 0.05.
 
-            length : int
-                length of the bitstring
+            length : int, optional
+                Length of the bitstring. Default 56.
 
-            chunk_s : int
-                size of the chunk of bits to be chosen together
+            chunk_s : int, optional
+                Size of the single chunk. Default 8.
         """
-        out_shape = [n_mut, *self.in_shape]
+        out_shape = [n_mut, *self._in_shape]
         self._gen_mut_masks(n_mut, **kwargs)
-        self.mutations = self.int2float(self._int_orig ^ self._mut_masks)
+        self.mutations = self._int2float(self._int_orig ^ self._mut_masks)
         self.mutations = np.nan_to_num(self.mutations, nan=1.0,
                                        posinf=100, neginf=-100)
-        if apply_selection:
-            selection_res = self.pseudo_selection(self.mutations, n_selections)
-            self.mutations = np.concatenate((self.mutations, selection_res))
-            out_shape[0] = n_mut * (n_selections + 1)
-            self.mutations = self.mutations.reshape(out_shape)
-            return self.mutations
         self.mutations = self.mutations.reshape(out_shape)
+        if apply_selection:
+            selection_res = self._pseudo_selection(self.mutations, n_selections)
+            self.mutations = np.concatenate((self.mutations, selection_res))
+            return self.mutations
         return self.mutations
 
 
+# TODO: remove this in the final version
 if __name__ == "__main__":
-    bit_ops = BitOps(np.array([0.234, -1.23, 12.625]))
-    print(bit_ops.mutate(n_mut=10, apply_selection=True, n_selections=2))
+    bit_ops_ = BitOps(np.array([0.234, -1.23, 12.625]))
+    print(bit_ops_.mutate(n_mut=3, apply_selection=True,
+                          n_selections=1, prob=0.01))
     # test_bits = BitOps(np.random.normal(0, 1000, 100000))
     # test_bits.mutate(n_mut=100, apply_selection=True, n_selections=10)
